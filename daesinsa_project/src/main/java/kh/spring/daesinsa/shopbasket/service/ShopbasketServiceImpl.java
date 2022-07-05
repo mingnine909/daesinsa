3166baa174c639d240b14db1ce2548b2705a6468
byeongguk93 <sensual12@naver.com>
2022-07-05 11:20:14 +0900

    220705 shopbasketserviceimpl 에러 수정

--- daesinsa_project/src/main/java/kh/spring/daesinsa/shopbasket/service/ShopbasketServiceImpl.java
+++ daesinsa_project/src/main/java/kh/spring/daesinsa/shopbasket/service/ShopbasketServiceImpl.java
@@ -6,67 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kh.spring.daesinsa.shopbasket.domain.Shopbasket;
-import kh.spring.daesinsa.shopping.domain.ProductQna;
-import kh.spring.daesinsa.shopping.domain.ProductReview;
-import kh.spring.daesinsa.shopping.domain.Shopping;
-import kh.spring.daesinsa.shopping.model.service.ShoppingService;
+
 
 @Service
-public class ShopbasketServiceImpl implements ShoppingService {
+public class ShopbasketServiceImpl implements ShopbasketService {
 
 	@Autowired
 	private Shopbasket dao;
 
 	@Override
-	public int selectCaTotal(String c_id) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public List<Shopping> selectListAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Shopping> selectListCa(Shopping shopping, int currentPage, int pageSize) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Shopping> selectListCaMin(Shopping shopping, int currentPage, int pageSize) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Shopping> selectListCaMax(Shopping shopping, int currentPage, int pageSize) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Shopping detailProduct(Shopping shopping) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int selectSearchTotal(String keyword) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public List<Shopping> searchProduct(String keyword, int currentPage, int pageSize) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<ProductQna> selectQnaList(String p_id) {
+	public List<Shopbasket> getShopBasketList(String c_id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
